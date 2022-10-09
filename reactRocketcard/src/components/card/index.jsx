309baf85aba045e4a09baa931968241b8fb1892f
Du-devBR <eduardo.ananiasdev@outlook.com>
2022-10-09 18:45:49 -0300

    corrigido rota para os componenst com o sass

---
+++ reactRocketcard/src/components/card/index.jsx
@@ -0,0 +1,9 @@
+import "../../style/components/card.sass"
+
+export function Card(){
+  return (
+    <div className="card">
+      <h1>Eduardo ananias</h1>
+    </div>
+  )
+}
---
+++ reactRocketcard/src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+
+import { Card } from '../../components/card/index'
+
+
+export function App() {
+
+  return (
+    <div className="App">
+      <h1>rocketcard</h1>
+      <Card />
+    </div>
+  )
+}
